66c84b39f2836e4e9e98aca20fe92dbb4a406f6b
Alex <alexander_kobebryan@hotmail.com>
2020-09-21 02:03:01 +0800

    Proxy

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,17 @@
 const  Sequelize  = require("sequelize");
 
-const sequelize = new Sequelize('user_db_table', 'admin', 'password',{
-  dialect: "mysql",
-  host: "35.247.145.163",
-});
+// const sequelize = new Sequelize('user_db_table', 'admin', 'password',{
+//   dialect: "mysql",
+//   host: "35.247.145.163",
+// });
 
+const sequelize = new Sequelize('user_db_table', 'admin', 'password', {
+  dialect: 'mysql',
+  host: '/cloudsql/35.247.145.163',
+  timestamps: false,
+  dialectOptions: {
+    socketPath: '/cloudsql/35.247.145.163'
+},
+});
 
 module.exports = sequelize;
